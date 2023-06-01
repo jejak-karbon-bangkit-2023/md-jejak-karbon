0d69d8ece0ee2854b67b1e155199a664739bb697
maodaa <12201510@bsi.ac.id>
2023-06-01 22:04:44 +0700

    add profile screen and edit profile, font

---
+++ app/src/main/java/com/jejakkarbon/ui/profile/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.jejakkarbon.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jejakkarbon.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
