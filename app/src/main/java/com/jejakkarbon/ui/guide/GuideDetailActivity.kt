c0ffc5ee897856d3c12616078f74a179b9eac275
fnnrzkn <nurrizkiirfan@gmail.com>
2023-06-08 14:51:02 +0700

    Update Activity Guide (Add guide screen title, add button category, add item for recycler view (guide title, guide category,guide avatar,guide date, round border,strings, logo, top menu)
    add guide detail

---
+++ app/src/main/java/com/jejakkarbon/ui/guide/GuideDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.jejakkarbon.ui.guide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jejakkarbon.R
+
+class GuideDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guide_detail)
+    }
+}
